66423c1c96082732c4fe96a535afa385ca582500
semachiah <p.varejcka@seznam.cz>
2025-01-18 19:39:30 +0100

    Update app.js

--- public/app.js
+++ public/app.js
@@ -36,6 +36,8 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  logger: true, // Povolit logování
+  debug: true   // Zahrnout ladící výstup
 });
 
 // API Routes
